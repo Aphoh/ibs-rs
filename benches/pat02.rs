7451a4314e83d79d9e7c53cd3bf8add07994b9d0
William Arnold <will748@gmail.com>
2020-08-06 17:12:59 -0700

    Try different methods of pat02 verification
    Signed-off-by: William Arnold <will748@gmail.com>

--- benches/pat02.rs
+++ benches/pat02.rs
@@ -6,7 +6,7 @@ use rand::Rng;
 pub fn criterion_pat02(c: &mut Criterion) {
     let mut rng = rand::thread_rng();
 
-    let (ta_secret, ta_pub_key) = setup(&mut rand::thread_rng());
+    let (ta_secret, ta_pub_key, g_const) = setup(&mut rand::thread_rng());
 
     for _ in 0..3 {
         let uid: [u8; 32] = rng.gen();
@@ -23,8 +23,12 @@ pub fn criterion_pat02(c: &mut Criterion) {
 
         let (u_sig, v_sig) = sign(msg, &k, &user_secret);
 
-        c.bench_function("pat02 verify", |b| {
-            b.iter(|| verify(msg, &u_sig, &v_sig, uid, &ta_pub_key))
+        c.bench_function("pat02 method 1 verify", |b| {
+            b.iter(|| verify_method_1(msg, &u_sig, &v_sig, uid, &ta_pub_key))
+        });
+
+        c.bench_function("pat02 method 2 verify", |b| {
+            b.iter(|| verify_method_2(msg, &u_sig, &v_sig, uid, &ta_pub_key, &g_const))
         });
     }
 }
--- src/pat02/mod.rs
+++ src/pat02/mod.rs
@@ -5,11 +5,12 @@
 
 use super::*;
 use rand::{CryptoRng, RngCore};
+use std::ops::Mul;
 
-pub fn setup<R: RngCore + CryptoRng>(rng: &mut R) -> (FieldElement, G2) {
+pub fn setup<R: RngCore + CryptoRng>(rng: &mut R) -> (FieldElement, G2, GT) {
     let ta_secret = FieldElement::random_using_rng(rng);
     let ta_pub = G2::generator().scalar_mul_const_time(&ta_secret);
-    (ta_secret, ta_pub)
+    (ta_secret, ta_pub, GT::ate_pairing(&G1::generator(), &G2::generator()))
 }
 
 pub fn extract<B: AsRef<[u8]>>(identity: B, ta_secret: &FieldElement) -> G1 {
@@ -24,7 +25,10 @@ pub fn sign<M: AsRef<[u8]>>(message: M, k: &FieldElement, user_secret: &G1) -> (
     (u, v)
 }
 
-pub fn verify<M, B>(message: M, u: &G2, v: &G1, identity: B, ta_pub_key: &G2) -> bool
+/// Verifies using `GT::ate_2_pairing` and multiplying `H_2(M)` by one of the generators, as well
+/// as multiplying `H_3(R)` by the user public key before computation. This avoids exponentiation
+/// in `GT`. Strangely, this is more efficient than the obvious method, used in `verify_method_2`.
+pub fn verify_method_1<M, B>(message: M, u: &G2, v: &G1, identity: B, ta_pub_key: &G2) -> bool
 where
     M: AsRef<[u8]>,
     B: AsRef<[u8]>,
@@ -40,6 +44,22 @@ where
     pair == uv_pair
 }
 
+/// Verifies by using a precomputed `e(P, Q)` and exponentiating in GT, using the obvious methods
+/// of computing
+pub fn verify_method_2<M, B>(message: M, u: &G2, v: &G1, identity: B, ta_pub_key: &G2, g_const: &GT) -> bool
+where
+    M: AsRef<[u8]>,
+    B: AsRef<[u8]>,
+{
+    let uv_pair = GT::ate_pairing(v, u);
+    let u_hash = hash_to_scalar(u.to_bytes(false));
+
+    let user_pub = hash_to_g1(identity);
+    let rhs = g_const.pow(&hash_to_scalar(message)) * GT::ate_pairing(&user_pub, &ta_pub_key.mul(u_hash));
+
+    rhs.eq(&uv_pair) 
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -47,27 +67,54 @@ mod tests {
     const IDENT: &str = "Duncan Edwards";
 
     #[test]
-    fn test_correctness() {
-        let (ta_secret, ta_pub_key) = setup(&mut rand::thread_rng());
+    fn test_correctness_method_1() {
+        let (ta_secret, ta_pub_key, _) = setup(&mut rand::thread_rng());
         let user_secret = extract(IDENT, &ta_secret);
 
         let k = hash_to_scalar("Random scalar");
         let (u_sig, v_sig) = sign(MSG, &k, &user_secret);
 
-        assert!(verify(MSG, &u_sig, &v_sig, IDENT, &ta_pub_key));
-        assert!(!verify(
+        assert!(verify_method_1(MSG, &u_sig, &v_sig, IDENT, &ta_pub_key));
+        assert!(!verify_method_1(
             "Incorrect message",
             &u_sig,
             &v_sig,
             IDENT,
             &ta_pub_key
         ));
-        assert!(!verify(
+        assert!(!verify_method_1(
             MSG,
             &u_sig,
             &v_sig,
             "Incorrect Identity",
             &ta_pub_key
+        ));
+    }
+
+    #[test]
+    fn test_correctness_method_2() {
+        let (ta_secret, ta_pub_key, g) = setup(&mut rand::thread_rng());
+        let user_secret = extract(IDENT, &ta_secret);
+
+        let k = hash_to_scalar("Random scalar");
+        let (u_sig, v_sig) = sign(MSG, &k, &user_secret);
+
+        assert!(verify_method_2(MSG, &u_sig, &v_sig, IDENT, &ta_pub_key, &g));
+        assert!(!verify_method_2(
+            "Incorrect message",
+            &u_sig,
+            &v_sig,
+            IDENT,
+            &ta_pub_key,
+            &g
+        ));
+        assert!(!verify_method_2(
+            MSG,
+            &u_sig,
+            &v_sig,
+            "Incorrect Identity",
+            &ta_pub_key,
+            &g
         ));
     }
 }
